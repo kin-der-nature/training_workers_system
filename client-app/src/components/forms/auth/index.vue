185f1adbcf2a6dd17d2a0153038387c4f0f0deeb
out_zubanova.ay <kin_der_nature@mail.ru>
2024-03-03 11:11:16 +0500

    add(client-app): created auth form

---
+++ client-app/src/components/forms/auth/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="auth_form">
+    <el-input v-model="model.name" placeholder="Введите логин" />
+    <el-input v-model="model.password" type="password" placeholder="Введите пароль" />
+  </div>
+</template>
+
+<script setup>
+import { defineModel } from 'vue';
+import { ElInput } from 'element-plus';
+
+const model = defineModel()
+
+</script>
+
+<style scoped>
+
+</style>
