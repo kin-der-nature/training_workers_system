4f7ff39ef7f3e5c79830ddc0ca3ca5b2b289ce2d
out_zubanova.ay <kin_der_nature@mail.ru>
2024-03-03 11:19:05 +0500

    add(client-app): created auth screen

---
+++ client-app/src/screen/auth/index.vue
@@ -0,0 +1,53 @@
+<script setup>
+import { ElCard, ElButton } from 'element-plus';
+import authForm from '../../components/forms/auth/index.vue';
+import { reactive } from 'vue';
+
+const usersAuth = reactive({})
+
+</script>
+
+<template>
+  <div class="auth_screen">
+    <el-card class="auth_card">
+      <div class="auth_title__block">
+        <h1>Войти</h1>
+      </div>
+      <auth-form class="auth_form" v-model="usersAuth" />
+      <div class="auth_button__block">
+        <el-button type="primary" size="large">
+          Войти
+        </el-button>
+      </div>
+    </el-card>
+  </div>
+</template>
+
+<style scoped>
+.auth_screen {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+}
+.auth_card {
+  max-width: 300px;
+  width: 100%;
+  padding: 20px;
+}
+.auth_title__block {
+  width: 100%;
+  margin: 20px 0;
+}
+.auth_form {
+  display: grid;
+  gap: 20px;
+}
+.auth_button__block {
+  width: 100%;
+  height: 100%;
+  margin: 20px 0;
+}
+</style>
