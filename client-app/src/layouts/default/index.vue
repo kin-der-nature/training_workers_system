39ef607d59a3c07f03b6505d7e5a022e7cb2cc15
out_zubanova.ay <kin_der_nature@mail.ru>
2024-03-03 12:13:56 +0500

    add(client-app): added sidebar for default layout

--- client-app/src/layouts/default/index.vue
+++ client-app/src/layouts/default/index.vue
@@ -1,14 +1,32 @@
 <template>
   <div class="default_layout">
+    <el-menu class="default_layout_sidebar_menu">
+      <el-menu-item>
+        Профиль
+      </el-menu-item>
+      <el-menu-item>
+        Курсы
+      </el-menu-item>
+      <el-menu-item>
+        Статистика
+      </el-menu-item>
+    </el-menu>
   </div>
 </template>
 
 <script setup>
+import { ElMenu, ElMenuItem } from 'element-plus'
 </script>
 
 <style scoped>
 .default_layout {
   display: flex;
   height: 100%;
+}
+.default_layout_sidebar_menu {
+  max-width: 300px;
+  width: 100%;
+  height: 100%;
+  padding: 20px 0;
 }
 </style>
