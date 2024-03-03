2fe63bfa090b2bae83bf83c0d3af85468e0d8e3f
out_zubanova.ay <kin_der_nature@mail.ru>
2024-03-03 11:44:05 +0500

    add(client-app): created home screen

--- client-app/src/router/index.js
+++ client-app/src/router/index.js
@@ -1,6 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-const routes = []
+const routes = [
+  {
+    name: 'auth',
+    path: '/',
+    component: import('../screen/auth/index.vue')
+  },
+  {
+    name: 'Home',
+    path: '/home',
+    component: import('../screen/home/index.vue')
+  }
+]
 
 const router = createRouter({
   history: createWebHistory(),
---
+++ client-app/src/screen/home/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home-screen">
+
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.home-screen {
+  width: 100%;
+}
+</style>
