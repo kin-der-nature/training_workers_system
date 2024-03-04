e31fdb1761ea4e334dcaae95d4b80cabdc43fdcc
out_zubanova.ay <kin_der_nature@mail.ru>
2024-03-04 16:22:46 +0500

    add(client-app): the default container for the preview image slot

--- client-app/src/components/card/index.vue
+++ client-app/src/components/card/index.vue
@@ -1,6 +1,9 @@
 <template>
   <el-card class="default_card">
     <slot name="preview-image">
+      <div class="preview_image_container">
+        <img class="preview-image">
+      </div>
     </slot>
   </el-card>
 </template>
@@ -10,5 +13,8 @@ import { ElCard } from 'element-plus';
 </script>
 
 <style scoped>
-
+.preview_image_container {
+  width: 100%;
+  height: 100%;
+}
 </style>
