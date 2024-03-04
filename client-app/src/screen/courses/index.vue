1ac0582705d7f923a57ee580a1da8d1f952217d6
out_zubanova.ay <kin_der_nature@mail.ru>
2024-03-04 10:13:17 +0500

    add(client-app): created courses screen

---
+++ client-app/src/screen/courses/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="courses_screen">
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.courses_card_container {
+  width: 100%;
+  height: 100%;
+  display: grid;
+  grid-template-columns: repeat(3, 2fr);
+  gap: 30px;
+}
+</style>
