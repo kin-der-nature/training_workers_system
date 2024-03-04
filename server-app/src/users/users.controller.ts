5cd392fa99e8867670ec56ea0d488f10183537dd
out_zubanova.ay <kin_der_nature@mail.ru>
2024-03-05 02:16:13 +0500

    add(server-app): created user controller

--- server-app/src/users/users.controller.ts
+++ server-app/src/users/users.controller.ts
@@ -1,7 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { createUserDto } from './dto/create_user.dto';
 
-@Controller('rest/users')
+@Controller('users')
 export class UsersController {
-  constructor(private service: UsersService) {}
+  constructor(private usersService: UsersService) {}
+
+  @Post()
+  create(@Body() userDto: createUserDto) {
+    return this.usersService.createUser(userDto);
+  }
+
+  @Get()
+  getAll() {
+    return this.usersService.getAllUsers();
+  }
 }
