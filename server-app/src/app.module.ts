34f2033d1b7d1647d3d40c8a19d1749582b72f26
out_zubanova.ay <kin_der_nature@mail.ru>
2024-03-04 22:49:51 +0500

    update(server-app): removed default module

--- server-app/src/app.module.ts
+++ server-app/src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: 'localhost',
+      port: 3306,
+      username: 'postqres',
+      password: 'admin12345',
+      database: 'test',
+      models: [],
+    }),
+
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
