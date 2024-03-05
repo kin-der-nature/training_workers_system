462d585ea572893d9c3d75600488b35f8e2cd1f1
out_zubanova.ay <kin_der_nature@mail.ru>
2024-03-05 15:36:24 +0500

    update(server-app): export roles service

--- server-app/src/roles/roles.module.ts
+++ server-app/src/roles/roles.module.ts
@@ -8,5 +8,6 @@ import { Roles } from './roles.model';
   imports: [SequelizeModule.forFeature([Roles])],
   controllers: [RolesController],
   providers: [RolesService],
+  exports: [RolesService],
 })
 export class RolesModule {}
