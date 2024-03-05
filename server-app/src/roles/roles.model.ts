037905e95a9bf91be3a616541dee70cd9a2c8141
out_zubanova.ay <kin_der_nature@mail.ru>
2024-03-05 16:08:02 +0500

    add(server-app): created roles model method getting user by login

---
+++ server-app/src/roles/roles.model.ts
@@ -0,0 +1,34 @@
+import {
+  Model,
+  Column,
+  DataType,
+  Table,
+  BelongsToMany,
+} from 'sequelize-typescript';
+import { UserRoles } from '../roles/user_roles.model';
+import { User } from 'src/users/user.model';
+
+interface createRoleDto {
+  name: string;
+  description: string;
+}
+
+@Table({ tableName: 'roles' })
+export class Roles extends Model<Roles, createRoleDto> {
+  @Column({
+    type: DataType.INTEGER,
+    unique: true,
+    autoIncrement: true,
+    primaryKey: true,
+  })
+  id: number;
+
+  @Column({ type: DataType.STRING, unique: true, allowNull: false })
+  name: string;
+
+  @Column({ type: DataType.STRING(200), allowNull: false })
+  description: string;
+
+  @BelongsToMany(() => User, () => UserRoles)
+  users: User[];
+}
--- server-app/src/users/users.service.ts
+++ server-app/src/users/users.service.ts
@@ -1,3 +1,4 @@
+import { RolesService } from './../roles/roles.service';
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 import { User } from './user.model';
@@ -8,17 +9,31 @@ export class UsersService {
   constructor(
     @InjectModel(User)
     private userRepository: typeof User,
+    private roleService: RolesService,
   ) {}
 
   async createUser(dto: createUserDto) {
     const user = await this.userRepository.create(dto);
-
-    return user;
+    const defaultRole = await this.roleService.getRoleByName('admin1');
+    await user.$set('roles', [defaultRole.id]);
+    user.roles = [defaultRole];
+    return defaultRole;
   }
 
   async getAllUsers() {
-    const users = await this.userRepository.findAll();
+    const users = await this.userRepository.findAll({ include: { all: true } });
 
     return users;
+  }
+
+  async getUserByLogin(v: string) {
+    const currentUser = await this.userRepository.findOne({
+      where: {
+        login: v,
+      },
+      include: { all: true },
+    });
+
+    return currentUser;
   }
 }
