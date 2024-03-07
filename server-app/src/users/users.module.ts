fc37bc0ac9943cd7b83ed2b75f97266bf406d717
out_zubanova.ay <kin_der_nature@mail.ru>
2024-03-07 22:36:18 +0500

    update(server-app): import roles module to user module

--- server-app/src/users/users.module.ts
+++ server-app/src/users/users.module.ts
@@ -1,12 +1,21 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { SequelizeModule } from '@nestjs/sequelize';
-import { User } from './user.model';
+import { User } from '../users/user.model';
+import { Roles } from '../roles/roles.model';
+import { UserRoles } from '../roles/user_roles.model';
+import { RolesModule } from '../roles/roles.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [SequelizeModule.forFeature([User])],
-  providers: [UsersService, UsersService],
-  controllers: [UsersController, UsersController],
+  controllers: [UsersController],
+  providers: [UsersService],
+  imports: [
+    SequelizeModule.forFeature([User, Roles, UserRoles]),
+    RolesModule,
+    forwardRef(() => AuthModule),
+  ],
+  exports: [UsersService],
 })
 export class UsersModule {}
