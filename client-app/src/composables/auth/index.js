96253936f7c14857b7e2d4bf42e0d2f0a7178bd5
out_zubanova.ay <kin_der_nature@mail.ru>
2024-03-10 00:20:14 +0500

    add(client-app): created method auth by params

--- client-app/src/composables/auth/index.js
+++ client-app/src/composables/auth/index.js
@@ -1,13 +1,24 @@
 import { reactive } from "vue"
+import { useUserStore } from "../../store/user"
+import { getTokenByAuthParamsRequest } from "../../api/user"
 
 export const useAuth = () => {
+
+  const userStore = useUserStore();
 
   const authParams = reactive({
     login: null,
     password: null
   })
 
+  const authByParams = async ({ login, password }) => {
+    const { data: { token } } = await getTokenByAuthParamsRequest({ login, password })
+
+    userStore.setToken(token);
+  }
+
   return {
-    authParams
+    authParams,
+    authByParams
   }
 }
