7f6de3909492655e8ca166046110c59a2d18a631
out_zubanova.ay <kin_der_nature@mail.ru>
2024-03-09 20:42:00 +0500

    add(client-app): created the method set state value user

--- client-app/src/store/user.js
+++ client-app/src/store/user.js
@@ -4,7 +4,12 @@ import { ref } from "vue";
 export const useUserStore = defineStore('user', () => {
   const user = ref(null);
 
+  const setUserState = (v) => {
+    user.value = v;
+  }
+
   return {
-    user
+    user,
+    setUserState
   }
 })
