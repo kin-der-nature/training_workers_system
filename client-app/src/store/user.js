7cfceb6cd05a16cf95998c1c2ff705f90a9a3221
out_zubanova.ay <kin_der_nature@mail.ru>
2024-03-09 21:03:02 +0500

    add(client-app): token state

--- client-app/src/store/user.js
+++ client-app/src/store/user.js
@@ -4,12 +4,15 @@ import { ref } from "vue";
 export const useUserStore = defineStore('user', () => {
   const user = ref(null);
 
+  const token = ref(localStorage.getItem('token'));
+
   const setUserState = (v) => {
     user.value = v;
   }
 
   return {
     user,
+    token,
     setUserState
   }
 })
