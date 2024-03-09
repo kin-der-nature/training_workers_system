d3f1e3f7a408183d86339305776a0ebe6105d77f
out_zubanova.ay <kin_der_nature@mail.ru>
2024-03-09 17:58:23 +0500

    add(server-app): method create test

--- server-app/src/tests/test.service.ts
+++ server-app/src/tests/test.service.ts
@@ -1,4 +1,18 @@
+import { createTestDto } from './dto/index.dto';
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { Test } from './test.model';
 
 @Injectable()
-export class TestService {}
+export class TestService {
+  constructor(
+    @InjectModel(Test)
+    private testRepository: typeof Test,
+  ) {}
+
+  createTest(dto: createTestDto) {
+    const result = this.testRepository.create(dto);
+
+    return result;
+  }
+}
