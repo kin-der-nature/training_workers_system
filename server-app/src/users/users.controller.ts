fea9459aa8f3af4f22fad25c9b5d6a7ed5931a3c
out_zubanova.ay <kin_der_nature@mail.ru>
2024-03-10 18:47:35 +0500

    add(server-app): end point getting user by token

--- server-app/src/users/users.controller.ts
+++ server-app/src/users/users.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Query, Post, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { createUserDto } from './dto/create_user.dto';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guards';
+import { userWithByCurrentParamsNotFound } from './utils/message';
 
 @Controller('users')
 export class UsersController {
@@ -17,5 +18,16 @@ export class UsersController {
   @UseGuards(JwtAuthGuard)
   getAll() {
     return this.usersService.getAllUsers();
+  }
+
+  @Get('get_by_token/')
+  async getByToken(@Query('token') token: string) {
+    const user = await this.usersService.getUserByToken(token);
+
+    if (!user) {
+      userWithByCurrentParamsNotFound();
+    }
+
+    return user;
   }
 }
