9879a38f5beeb84c178d124b475927bd2972a037
out_zubanova.ay <kin_der_nature@mail.ru>
2024-03-11 17:41:54 +0500

    add(server-app): created associa "has many" with model "quenstion_variant", for get all variants

--- server-app/src/questions/questions.model.ts
+++ server-app/src/questions/questions.model.ts
@@ -1,5 +1,6 @@
-import { Column, DataType, Model, Table } from 'sequelize-typescript';
+import { Column, DataType, HasMany, Model, Table } from 'sequelize-typescript';
 import { createQuestion } from './dto/index.dto';
+import { Questions_variant } from './variants.model';
 
 @Table({ tableName: 'questions' })
 export class Questions extends Model<Questions, createQuestion> {
@@ -16,4 +17,7 @@ export class Questions extends Model<Questions, createQuestion> {
     allowNull: false,
   })
   name: string;
+
+  @HasMany(() => Questions_variant)
+  variants: Questions_variant[];
 }
