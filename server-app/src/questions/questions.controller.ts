7d3a5609c319b056f470e43061e7a681829487d8
out_zubanova.ay <kin_der_nature@mail.ru>
2024-03-11 19:24:56 +0500

    add(server-app): created end point getting all question

--- server-app/src/questions/questions.controller.ts
+++ server-app/src/questions/questions.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { QuestionsService } from './questions.service';
 
 @Controller('questions')
 export class QuestionsController {
   constructor(private readonly questionsService: QuestionsService) {}
+
+  @Get()
+  async getAll() {
+    const result = await this.questionsService.getAllQuestions();
+    return result.map(({ id, name, variants }) => ({ id, name, variants }));
+  }
 }
