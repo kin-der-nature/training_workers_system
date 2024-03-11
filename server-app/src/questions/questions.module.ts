22229ad9ac49233a796caf0d741555a84237bdc6
out_zubanova.ay <kin_der_nature@mail.ru>
2024-03-11 10:26:41 +0500

    add(server-app): import questions model to questions module

--- server-app/src/questions/questions.module.ts
+++ server-app/src/questions/questions.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { QuestionsService } from './questions.service';
 import { QuestionsController } from './questions.controller';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { Questions } from './questions.model';
 
 @Module({
   controllers: [QuestionsController],
+  imports: [SequelizeModule.forFeature([Questions])],
   providers: [QuestionsService],
 })
 export class QuestionsModule {}
