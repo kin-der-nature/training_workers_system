2e34fdf362466f49053259241559febdab195e70
out_zubanova.ay <kin_der_nature@mail.ru>
2024-03-11 09:36:41 +0500

    add(client-app): created method logout user

--- client-app/src/utils/user/index.js
+++ client-app/src/utils/user/index.js
@@ -1,5 +1,8 @@
 import { useUserStore } from "../../store/user"
+import router from '../../router/index.js'
 
 export const logout = () => {
   const userStore = useUserStore();
+  userStore.resetToken();
+  router.push('/');
 }
