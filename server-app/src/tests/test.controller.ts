4f6cda5e021b33cc9081d34f29987bdca896b173
out_zubanova.ay <kin_der_nature@mail.ru>
2024-03-12 10:29:27 +0500

    add(server-app): created end point for getting all tests

--- server-app/src/tests/test.controller.ts
+++ server-app/src/tests/test.controller.ts
@@ -1,10 +1,15 @@
 import { TestService } from 'src/tests/test.service';
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { createTestDto } from './dto/index.dto';
 
 @Controller('tests')
 export class TestController {
   constructor(private testService: TestService) {}
+
+  @Get()
+  getAll() {
+    return this.testService.getTestsAll();
+  }
 
   @Post()
   create(@Body() dto: createTestDto) {
