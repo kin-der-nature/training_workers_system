7c1b082d2aa18b8e911542b3996cc3ecdd43a481
out_zubanova.ay <kin_der_nature@mail.ru>
2024-03-18 22:43:30 +0500

    update(server-app): migrated schema getting the question variants, to separate state

--- server-app/src/tests/schema/index.ts
+++ server-app/src/tests/schema/index.ts
@@ -1,14 +1,18 @@
 import { Questions } from 'src/questions/questions.model';
 import { Questions_variant } from 'src/questions/variants.model';
 
+export const questionVariant = {
+  model: Questions_variant,
+  attributes: ['id', 'name'],
+};
+
 export const test = {
   model: Questions,
   all: true,
   nested: true,
   include: [
     {
-      model: Questions_variant,
-      attributes: ['id', 'name'],
+      ...questionVariant,
     },
   ],
   attributes: ['id', 'name'],
