f0c4a745bb81b2a20d85e7979cdd5ff2d0bf9aae
out_zubanova.ay <kin_der_nature@mail.ru>
2024-03-18 22:51:29 +0500

    add(server-app): created the helper method compute percent

--- server-app/src/helpers/numbers/index.ts
+++ server-app/src/helpers/numbers/index.ts
@@ -1,3 +1,7 @@
 export const getSumNumbers = (numbers: number[]): number => {
   return numbers.reduce((acc, item) => (acc = acc + item), 0);
 };
+
+export const computePercent = (a: number, b: number): any => {
+  return (a / b) * 100;
+};
