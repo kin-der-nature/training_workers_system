971ddf5ba54dafd46bd29295b96cdfce759e3ef0
out_zubanova.ay <kin_der_nature@mail.ru>
2024-03-18 22:57:44 +0500

    add(server-app): created the type responses of test

--- server-app/src/tests/dto/index.dto.ts
+++ server-app/src/tests/dto/index.dto.ts
@@ -1,4 +1,11 @@
+import { responsesToQuestion } from 'src/questions/dto/index.dto';
+
 export interface createTestDto {
   readonly name: string;
   readonly questions: number[];
 }
+
+export type responsesOfTest = {
+  id: number;
+  questions: responsesToQuestion[];
+};
