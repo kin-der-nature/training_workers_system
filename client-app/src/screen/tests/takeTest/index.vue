90d776ee83e838af7282d49ac34b8fa59b142df9
out_zubanova.ay <kin_der_nature@mail.ru>
2024-03-19 10:28:32 +0500

    add(client-app): getting test by id param

--- client-app/src/screen/tests/takeTest/index.vue
+++ client-app/src/screen/tests/takeTest/index.vue
@@ -1,13 +1,22 @@
 <template>
   <div class="take_test_screen">
-
   </div>
 </template>
 
 <script setup>
+import { computed, onMounted } from 'vue';
+import { useTakeTest } from '../../../composables/tests/takeTest';
+import { useRoute } from 'vue-router';
+
+const { test, getTestById } = useTakeTest();
+const route = useRoute()
+
+const testId = computed(() => route.params.id)
+
+onMounted(() => {
+  getTestById(testId)
+})
 
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
