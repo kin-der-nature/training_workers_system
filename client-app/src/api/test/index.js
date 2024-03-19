c84c2081e7339b1dd7aaa79d03a1c2e86211164b
out_zubanova.ay <kin_der_nature@mail.ru>
2024-03-19 09:34:48 +0500

    add(client-app): created the method send request getting test by id

--- client-app/src/api/test/index.js
+++ client-app/src/api/test/index.js
@@ -3,3 +3,11 @@ import { httpInctance } from ".."
 export const getTestsRequest = () => {
   return httpInctance.get('tests/');
 }
+
+export const getTestByIdRequest = (dto) => {
+  const params = {
+    id: dto
+  }
+
+  return httpInctance.get('tests/one/', { params })
+}
