c06229ebfb8f8cfddf55fb2117bd24402baea6d8
out_zubanova.ay <kin_der_nature@mail.ru>
2024-03-19 11:46:47 +0500

    add(client-app): rendered the name of current question

--- client-app/src/screen/tests/takeTest/index.vue
+++ client-app/src/screen/tests/takeTest/index.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="take_test_screen">
+    <div class="question_container">
+      <div class="question_title_block">
+        <h1 class="question_title">
+          {{ currentQuestion?.name }}
+        </h1>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -8,7 +15,8 @@ import { computed, onMounted } from 'vue';
 import { useTakeTest } from '../../../composables/tests/takeTest';
 import { useRoute } from 'vue-router';
 
-const { test, getTestById } = useTakeTest();
+const { test, currentQuestion, getTestById } = useTakeTest();
+
 const route = useRoute()
 
 const testId = computed(() => route.params.id)
@@ -19,4 +27,15 @@ onMounted(() => {
 
 </script>
 
-<style scoped></style>
+<style scoped>
+.take_test_screen {
+  height: 100%;
+}
+
+.take_test_variants_container {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  margin: 10px 0;
+}
+</style>
