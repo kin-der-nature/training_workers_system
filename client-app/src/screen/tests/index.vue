f1abd00afa1efe8f431199bf07b4ce10105b766c
out_zubanova.ay <kin_der_nature@mail.ru>
2024-03-19 08:57:21 +0500

    add(client-app): button redirect to take test for the card "test"

--- client-app/src/screen/tests/index.vue
+++ client-app/src/screen/tests/index.vue
@@ -5,17 +5,26 @@
         v-for="test in tests.value"
         :key="test.id" :title="test.name"
         :preview-image=" { src: '../../../public/vite.svg', alt: 'test' }"
-      />
+      >
+      <template #description>
+        <el-button @click="router.push(`tests/${test.id}`)">
+          Пройти тест
+        </el-button>
+      </template>
+      </default-card>
     </div>
   </div>
 </template>
 
 <script setup>
 import { onMounted } from 'vue';
+import { useRouter } from 'vue-router';
 import { useTests } from '../../composables/tests';
 import defaultCard from '../../components/card/index.vue';
 
 const { tests, getTests } = useTests()
+
+const router = useRouter()
 
 onMounted(() => getTests())
 </script>
@@ -24,6 +33,7 @@ onMounted(() => getTests())
 .tests-list-container {
   width: 100%;
   display: grid;
+  gap: 40px;
   grid-template-columns: repeat(3, 1fr);
 }
 </style>
