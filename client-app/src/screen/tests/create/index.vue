7c21a05117d708fa5929d384160e1f5449895d3d
out_zubanova.ay <kin_der_nature@mail.ru>
2024-04-06 10:37:36 +0500

    add(client-app): added the form of the test

--- client-app/src/screen/tests/create/index.vue
+++ client-app/src/screen/tests/create/index.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="create_tests_screen">
-
+    <test-form v-model="form" size="large" />
   </div>
 </template>
 
 <script setup>
+import { reactive } from 'vue';
+import TestForm from '../../../components/forms/tests/index.vue'
+
+const form = reactive({})
 
 </script>
 
