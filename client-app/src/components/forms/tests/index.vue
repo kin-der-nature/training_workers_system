be21346c3c3a965fb56fcd43318aadfdf27869b0
out_zubanova.ay <kin_der_nature@mail.ru>
2024-04-06 12:15:50 +0500

    update(client-app): removed parent of component for elements of form

--- client-app/src/components/forms/tests/index.vue
+++ client-app/src/components/forms/tests/index.vue
@@ -1,14 +1,12 @@
 <template>
-  <el-form>
     <el-form-item>
       <el-input v-model="model.name" placeholder="Введите название" />
     </el-form-item>
-  </el-form>
 </template>
 
 <script setup>
 import { defineModel } from 'vue';
-import { ElForm, ElInput } from 'element-plus';
+import { ElInput } from 'element-plus';
 
 const model = defineModel()
 
