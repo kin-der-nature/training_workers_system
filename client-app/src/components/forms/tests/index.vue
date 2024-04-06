1f9f8ee1fa1e66cd6ee4e20edce9520362425bd4
out_zubanova.ay <kin_der_nature@mail.ru>
2024-04-06 10:15:34 +0500

    add(client-app): created the field input name

--- client-app/src/components/forms/tests/index.vue
+++ client-app/src/components/forms/tests/index.vue
@@ -1,11 +1,16 @@
 <template>
   <el-form>
-
+    <el-form-item>
+      <el-input v-model="model.name" placeholder="Введите название" />
+    </el-form-item>
   </el-form>
 </template>
 
 <script setup>
-import { ElForm } from 'element-plus';
+import { defineModel } from 'vue';
+import { ElForm, ElInput } from 'element-plus';
+
+const model = defineModel()
 
 </script>
 
