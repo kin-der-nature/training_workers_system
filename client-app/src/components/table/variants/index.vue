268462c8ea9f4f068b777eaae829419a95a466aa
out_zubanova.ay <kin_der_nature@mail.ru>
2024-04-08 20:47:21 +0500

    add(client-app): added column rendered index

--- client-app/src/components/table/variants/index.vue
+++ client-app/src/components/table/variants/index.vue
@@ -1,5 +1,6 @@
 <template>
   <el-table>
+    <el-table-column type="index" />
     <el-table-column prop="name" label="Вариант" />
     <el-table-column prop="counte" label="Баллы" />
   </el-table>
