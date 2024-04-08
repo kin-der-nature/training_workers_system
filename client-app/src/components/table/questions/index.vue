befe09c970936770ccbcd1301bf10f596c000fe1
out_zubanova.ay <kin_der_nature@mail.ru>
2024-04-09 02:26:40 +0500

    add(client-app): renderered of variants of question by variants table component to on the expanded the row

--- client-app/src/components/table/questions/index.vue
+++ client-app/src/components/table/questions/index.vue
@@ -1,6 +1,11 @@
 <template>
   <el-table>
     <el-table-column type="selection" />
+    <el-table-column type="expand">
+      <template #default="{ row: { variants } }">
+        <variants-table border :data="variants" />
+      </template>
+    </el-table-column>
     <el-table-column prop="name" label="Вопрос" />
     <el-table-column label="Баллы">
       <template #default="{ row: { variants } }">
@@ -12,6 +17,7 @@
 
 <script setup>
 import { ElTable, ElTableColumn } from 'element-plus';
+import VariantsTable from '../../../components/table/variants/index.vue'
 import { getSummaryAllScore } from '../../../utils/questions/index.js'
 
 </script>
