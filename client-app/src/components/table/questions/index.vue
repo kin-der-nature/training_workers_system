e8f3fde9e89e55c944ed04b7643e9c25a0f5cf22
out_zubanova.ay <kin_der_nature@mail.ru>
2024-04-08 18:41:16 +0500

    add(client-app):  output a the question sum scores  on column "question"

--- client-app/src/components/table/questions/index.vue
+++ client-app/src/components/table/questions/index.vue
@@ -2,12 +2,16 @@
   <el-table>
     <el-table-column prop="name" label="Вопрос" />
     <el-table-column label="Баллы">
+      <template #default="{ row: { variants } }">
+        {{ getSummaryAllScore(variants) }}
+      </template>
     </el-table-column>
   </el-table>
 </template>
 
 <script setup>
 import { ElTable, ElTableColumn } from 'element-plus';
+import { getSummaryAllScore } from '../../../utils/questions/index.js'
 
 </script>
 
