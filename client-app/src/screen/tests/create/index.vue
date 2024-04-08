cf44e6c6712b8e826f106a63d9b48129edadfed0
out_zubanova.ay <kin_der_nature@mail.ru>
2024-04-08 10:27:29 +0500

    add(client-app): installed of parent component el-form for test form

--- client-app/src/screen/tests/create/index.vue
+++ client-app/src/screen/tests/create/index.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="create_tests_screen">
-    <test-form v-model="form" size="large" />
+    <el-form size="large">
+      <test-form v-model="form" />
+    </el-form>
   </div>
 </template>
 
 <script setup>
 import { reactive } from 'vue';
+import { ElForm } from 'element-plus';
 import TestForm from '../../../components/forms/tests/index.vue'
 
 const form = reactive({})
