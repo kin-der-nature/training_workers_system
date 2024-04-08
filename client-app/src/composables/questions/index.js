d978edfe17d6b9b82d16475825d21dbb5b60cd68
out_zubanova.ay <kin_der_nature@mail.ru>
2024-04-08 15:32:18 +0500

    add(client-app): created the method getting questions

--- client-app/src/composables/questions/index.js
+++ client-app/src/composables/questions/index.js
@@ -1,12 +1,21 @@
 import { reactive } from "vue";
+import { getQuestionRequest } from "../../api/question";
 
-export const userQuestions = () => {
+export const useQuestions = () => {
   const questions = reactive({
     value: null,
     loading: false
   })
 
+  const getQuestions = async () => {
+    questions.loading = true;
+    const { data } = await getQuestionRequest();
+    questions.value = data;
+    questions.loading = false;
+  }
+
   return {
-    questions
+    questions,
+    getQuestions
   }
 }
