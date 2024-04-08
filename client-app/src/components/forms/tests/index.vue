72231ccb2c4ba6e2226566e6a655fd5b955aa27e
out_zubanova.ay <kin_der_nature@mail.ru>
2024-04-08 11:21:56 +0500

    add(client-app): added item of the form the table of questions

--- client-app/src/components/forms/tests/index.vue
+++ client-app/src/components/forms/tests/index.vue
@@ -2,11 +2,15 @@
     <el-form-item>
       <el-input v-model="model.name" placeholder="Введите название" />
     </el-form-item>
+    <el-form-item>
+      <questions-table class="test-table" />
+    </el-form-item>
 </template>
 
 <script setup>
 import { defineModel } from 'vue';
-import { ElInput } from 'element-plus';
+import { ElInput, ElFormItem } from 'element-plus';
+import QuestionsTable from '../../table/questions/index.vue';
 
 const model = defineModel()
 
