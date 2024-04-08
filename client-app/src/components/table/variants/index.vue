6fdb0c44b8214ee7f9bafbd7f8e49d876eb7ecd5
out_zubanova.ay <kin_der_nature@mail.ru>
2024-04-08 11:54:25 +0500

    add(client-app): added column "question"

--- client-app/src/components/table/variants/index.vue
+++ client-app/src/components/table/variants/index.vue
@@ -1,10 +1,11 @@
 <template>
   <el-table>
+    <el-table-column prop="name" label="Вариант" />
   </el-table>
 </template>
 
 <script setup>
-import { ElTable } from 'element-plus';
+import { ElTable, ElTableColumn } from 'element-plus';
 
 </script>
 
