c45fa4261a15597d8f00cdb43abb2003600b10dd
out_zubanova.ay <kin_der_nature@mail.ru>
2024-04-10 03:23:22 +0500

    add(cliient-app): created the method send request to creating question

--- client-app/src/api/question/index.js
+++ client-app/src/api/question/index.js
@@ -3,3 +3,7 @@ import { httpInctance } from ".."
 export const getQuestionRequest = () => {
   return httpInctance.get('questions/')
 }
+
+export const createQuestionRequest = (dto) => {
+  return httpInctance.post('questions/', dto);
+}
