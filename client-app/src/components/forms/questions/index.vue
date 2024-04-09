2fa3a575d1f7f6afd1b7049ef8300c47ba1745fd
out_zubanova.ay <kin_der_nature@mail.ru>
2024-04-09 12:39:31 +0500

    add(client-app): added item the form variant component

--- client-app/src/components/forms/questions/index.vue
+++ client-app/src/components/forms/questions/index.vue
@@ -2,10 +2,20 @@
   <el-form-item>
     <el-input placeholder="Введите вопрос" />
   </el-form-item>
+  <el-form-item>
+    <variant-form v-model="variant" />
+  </el-form-item>
 </template>
 
 <script setup>
 import { ElInput, ElFormItem } from 'element-plus';
+import VariantForm from '../variants/index.vue'
+import { reactive } from 'vue';
+
+const variant = reactive({
+  variant: null,
+  score: 0
+})
 
 </script>
 
