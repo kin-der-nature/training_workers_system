21630527a613b316c793e126acba8c29960371e7
out_zubanova.ay <kin_der_nature@mail.ru>
2024-04-09 13:33:02 +0500

    update(client-app): replaced variant param to "name", of binding to component input the score

--- client-app/src/components/forms/variants/index.vue
+++ client-app/src/components/forms/variants/index.vue
@@ -1,6 +1,6 @@
 <template>
   <el-form-item>
-    <el-input v-model="model.variant" placeholder="Введите вариант" />
+    <el-input v-model="model.name" placeholder="Введите вариант" />
   </el-form-item>
   <el-form-item>
     <el-input-number v-model="model.counte" :min="0" />
