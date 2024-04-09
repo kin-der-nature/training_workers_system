b30ecc80f69405f3b3911ac703a6eac404d283eb
out_zubanova.ay <kin_der_nature@mail.ru>
2024-04-09 11:43:13 +0500

    add(client-app): initilization the variant form component

---
+++ client-app/src/components/forms/questions/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <el-form-item>
+    <el-input placeholder="Введите вопрос" />
+  </el-form-item>
+</template>
+
+<script setup>
+import { ElInput, ElFormItem } from 'element-plus';
+
+</script>
+
+<style scoped>
+</style>
