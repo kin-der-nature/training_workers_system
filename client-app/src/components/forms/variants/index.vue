b77ef38c23546897fc0f2004e90f3c49c7b88bad
out_zubanova.ay <kin_der_nature@mail.ru>
2024-04-09 11:02:15 +0500

    add(client-app): form item the input score

--- client-app/src/components/forms/variants/index.vue
+++ client-app/src/components/forms/variants/index.vue
@@ -2,10 +2,13 @@
   <el-form-item>
     <el-input placeholder="Введите вариант" />
   </el-form-item>
+  <el-form-item>
+    <el-input-number :min="0" />
+  </el-form-item>
 </template>
 
 <script setup>
-import { ElFormItem, ElInput } from 'element-plus';
+import { ElFormItem, ElInput, ElInputNumber } from 'element-plus';
 
 </script>
 
