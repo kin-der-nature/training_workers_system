c60896e4689e3e58c3ca771e890535882b0f55b2
out_zubanova.ay <kin_der_nature@mail.ru>
2024-04-10 06:13:41 +0500

    add(client-app): form item, button creating question

--- client-app/src/components/sections/questions/create/index.vue
+++ client-app/src/components/sections/questions/create/index.vue
@@ -1,10 +1,16 @@
 <template>
   <div>
     <question-form v-model="question" />
+    <el-form-item>
+      <el-button type="primary">
+        Создать
+      </el-button>
+    </el-form-item>
   </div>
 </template>
 
 <script setup>
+import { ElFormItem, ElButton } from 'element-plus';
 import questionForm from '@/components/forms/questions/index.vue'
 import { reactive } from 'vue';
 
