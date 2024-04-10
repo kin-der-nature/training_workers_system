22c79c2348a0f957a1eeaf2c62e5b47c00449d89
out_zubanova.ay <kin_der_nature@mail.ru>
2024-04-10 05:21:51 +0500

    add(client-app): added question form

--- client-app/src/components/modal/question/index.vue
+++ client-app/src/components/modal/question/index.vue
@@ -1,11 +1,12 @@
 <template>
   <el-dialog>
-
+    <question-section />
   </el-dialog>
 </template>
 
 <script setup>
 import { ElDialog } from 'element-plus';
+import QuestionSection from '@/components/sections/questions/create/index.vue'
 
 </script>
 
